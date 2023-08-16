60cd44c2aaa4fb8281580d55d50da9f906bb9d50
Seinzzz <huseinzidane29@gmail.com>
2023-08-17 00:33:54 +0700

    add some command and change config with dotenv

---
+++ src/commands.js
@@ -0,0 +1,26 @@
+//File ini berisi seluruh perintah untuk bot dan keterangan pada setiap perintah
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+  {
+    name: 'hello',
+    description: 'Get a warm greeting!',
+  },
+  {
+    name: 'info',
+    description: 'About a bot',
+  },
+  {
+    name: 'author',
+    description: `Who's the ceator?`,
+  },
+  {
+    name: 'help',
+    description: `Commands you can do with BOT`,
+  },
+];
+
+export default commands;
--- src/index.js
+++ src/index.js
@@ -1,54 +1,92 @@
 // import Discord from "discord.js";
-//token bot 
+//token bot
 // Require the necessary discord.js classes
-const { Client, Events, REST, GatewayIntentBits, Routes } = require('discord.js');
-const { TOKEN, CLIENT_ID } = require('./config.json');
+import 'dotenv/config';
+import commands from './commands.js';
+import { authorEmbed, commandsHelpEmbed } from './embeds.js';
+import {
+  Client,
+  Events,
+  REST,
+  IntentsBitField,
+  GatewayIntentBits,
+  Routes,
+} from 'discord.js';
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-  {
-    name: 'hello',
-    description: 'Replies with Holla!',
-  },
-];
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    // IntentsBitField.Flags.GuildMembers,
+    // IntentsBitField.Flags.GuildMessages,
+    // IntentsBitField.Flags.MessageContent,
+  ],
+});
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-
-const rest = new REST({ version: '10' }).setToken(TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 try {
   console.log('Started refreshing application (/) commands.');
 
-  rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+  rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+    body: commands,
+  });
 
   console.log('Successfully reloaded application (/) commands.');
 } catch (error) {
   console.error(error);
 }
 
+client.on('interactionCreate', async (c) => {
+  if (!c.isChatInputCommand()) return;
+  // console.log(c);
+  //get commandName
+  let inputCommand = c.commandName;
 
-client.on('interactionCreate', async interaction => {
-  console.log("workkk");
-  if (!interaction.isChatInputCommand()) return;
+  switch (inputCommand) {
+    case 'ping':
+      console.log('workkk');
+      await c.reply({ content: 'Pong!', ephemeral: true });
+      break;
+    case 'hello':
+      await c.reply({
+        content: `HAI ${c.user.globalName} TOTT!!`,
+        ephemeral: true,
+      });
+      break;
+    case 'info':
+      await c.reply(`Bot ini dibuat dengan sedikit abstrak.`);
+      break;
+    case 'author':
+      await c.reply({ embeds: [authorEmbed], ephemeral: true });
+      break;
+    case 'help':
+      await c.reply({ embeds: [commandsHelpEmbed], ephemeral: true });
+      break;
+    default:
+      break;
+  }
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('HAI JINGKONTOTT!!');
-  }
-  if (interaction.commandName === 'hello') {
-    await interaction.reply('Hai!');
-  }
+  // if (c.commandName === 'ping') {
+  //   await c.reply({ content: 'Pong!', ephemeral: true });
+  // } else if (c.commandName === `hello`) {
+  //   await c.reply({
+  //     content: `HAI ${c.user.globalName} TOTT!!`,
+  //     ephemeral: true,
+  //   });
+  // } else if (c.commandName === `info`) {
+  //   await c.reply(`Bot ini dibuat dengan sedikit abstrak.`);
+  // } else if (c.commandName === `author`) {
+  //   // await c.reply(`Developed by: WollfKiller.org`);
+  //   await c.reply({ embeds: [exampleEmbed], ephemeral: true });
+  // }
 });
 
 // Log in to Discord with your client's token
-client.login(TOKEN);
+client.login(process.env.TOKEN);
